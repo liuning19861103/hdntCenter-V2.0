fd11cbb89af7b7c5f1ff64b3b22a5b6e293405d9
liuning19861103 <liuning1898@hotmail.com>
2020-08-24 18:11:38 +0800

    第二次更新

---
+++ Config/SysKeyWord.h
@@ -0,0 +1,197 @@
+/***************************************************************** 
+*  @brief:        
+*  @File:          
+*  @Project:    
+*  @Author:      
+*  @Date:         
+*  @CopyRight:   
+*  @Version:      
+*  @Description:  
+*****************************************************************/
+#ifndef __SYSKEYWORD_H__
+#define __SYSKEYWORD_H__
+
+/*define the key*/
+#define DEV_NAME  0
+#define SWID      1
+#define RES1      2
+#define RES2      3
+/*define the raw value*/
+#define GX_R      4
+#define GY_R      5
+#define GZ_R      6
+#define AX1_R     7
+#define AY1_R     8
+#define AZ1_R     9
+#define AX2_R     10
+#define AY2_R     11
+#define AZ2_R     12
+#define MX_R      13
+#define MY_R      14
+#define MZ_R      15
+#define GX_T      16
+#define GY_T      17
+#define GZ_T      18
+#define AX1_T     19
+#define AY1_T     20
+#define AZ1_T     21
+#define AX2_T     22
+#define AY2_T     23
+#define AZ2_T     24
+#define MX_T      25
+#define MY_T      26
+#define MZ_T      27
+#define TEMP      28
+/*define the rough calibration*/
+#define GX_RC     29
+#define GY_RC     30
+#define GZ_RC     31
+#define AX1_RC    32
+#define AY1_RC    33
+#define AZ1_RC    34
+#define AX2_RC    35
+#define AY2_RC    36
+#define AZ2_RC    37
+#define MX_RC     38
+#define MY_RC     39
+#define MZ_RC     40
+/*define the tn calibration value*/
+#define GX_TN     41
+#define GY_TN     42
+#define GZ_TN     43
+#define AX1_TN    44
+#define AY1_TN    45
+#define AZ1_TN    46
+#define AX2_TN    47
+#define AY2_TN    48
+#define AZ2_TN    49
+#define MX_TN     50
+#define MY_TN     51
+#define MZ_TN     52
+/*define the sf calibration value*/
+#define GX_SF     53
+#define GY_SF     54
+#define GZ_SF     55
+#define AX1_SF    56
+#define AY1_SF    57
+#define AZ1_SF    58
+#define AX2_SF    59
+#define AY2_SF    60
+#define AZ2_SF    61
+#define MX_SF     62
+#define MY_SF     63
+#define MZ_SF     64
+/*define the cr*/
+#define GX_CR     65
+#define GY_CR     66
+#define GZ_CR     67
+#define AX1_CR    68
+#define AY1_CR    69
+#define AZ1_CR    70
+#define AX2_CR    71
+#define AY2_CR    72
+#define AZ2_CR    73
+#define MX_CR     74
+#define MY_CR     75
+#define MZ_CR     76
+
+#define THETA     77
+#define PHI       78
+#define GAMMA     79
+#define INSPE     80
+#define INSPN     81
+#define INSPD     82
+#define INSVE     83   //20141001增加速度输出结果
+#define INSVN     84
+#define INSVD     85
+
+/*teh ADC channel*/
+#define ADC0V     86
+#define ADC1V     87
+#define ADC2V     88
+#define ADC3V     89
+#define ADC4V     90
+#define ADC5V     91
+#define ADC6V     92
+#define ADC7V     93
+#define ADC8V     94
+#define ADC9V     95
+#define ADC10V    96
+#define ADC11V    97
+#define ADC12V    98
+#define ADC13V    99
+#define ADC14V    100
+#define ADC15V    101
+
+#define GX_O      102
+#define GY_O      103
+#define GZ_O      104
+#define AX1_O     105
+#define AY1_O     106
+#define AZ1_O     107
+#define AX2_O     108
+#define AY2_O     109
+#define AZ2_O     110
+#define MX_O      111
+#define MY_O      112
+#define MZ_O      113
+
+#define GPS_LAT   114
+#define GPS_LONGT 115
+#define GPS_ALT   116
+#define GPS_VE    117
+#define GPS_VN    118
+#define GPS_VD    119
+#define GPS_SATN  120
+#define GPS_PDOP  121
+#define GPS_NAVTYPE 122
+#define GPS_DAYSECOND 123
+#define GPS_DESX  124
+#define GPS_DESY  125
+#define GPS_DESZ  126
+#define GPS_VX    127
+#define GPS_VY    128
+#define GPS_VZ    129
+
+#define HX0T1     130
+#define HX0T2     131
+#define HY0T1     132
+#define HY0T2     133
+#define HZ0T1     134
+#define HZ0T2     135
+#define HS0T1     136
+#define HS0T2     137
+#define HY0T1S    138
+#define HY0T2S    139
+#define HS0T1Y    140
+#define HS0T2Y    141
+
+#define MX        142
+#define MY        143
+#define MZ        144
+#define MX_C      145
+#define MY_C      146
+#define MZ_C      147
+#define MY_MAX0   148
+#define MY_MAX1   149
+#define MY_MIN    150
+#define MS        151
+#define MS_C      152
+#define MZ_MAX0   153
+#define MZ_MAX1   154
+#define MZ_MIN    155
+
+#define MODE      156
+#define TIME      157
+
+#define GPS_POSSTA 158
+#define GPS_PN    159
+#define GPS_PE    160
+#define GPS_PD    161
+
+#define VERSION   162
+
+#define OUT_CH_NUM 163
+
+#endif
+
