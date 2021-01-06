0d857eb8d1e259e3b8ec364a7459c11d81ca3883
liuning19861103 <liuning1898@hotmail.com>
2021-01-06 09:12:20 +0800

    liuning 202101060912 bj

---
+++ Ext/TempFolder/Res_Setting_1.h
@@ -0,0 +1,12 @@
+//预留报文宏定义
+#ifndef	__RESERVE_H__
+#define	__RESERVE_H__
+  #define  Res1   0
+  #define  Res2   1
+  #define  Res3   2
+  #define  Res4   3
+  #define  Res5   4
+  #define  Res6   5
+  #define  Res7   6
+  #define  Res8   7
+#endif
